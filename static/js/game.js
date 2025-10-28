532fdee5d737f2f2e4fe87d5fc191f8ac78ee0bf
vasilisliapas <102989989+vasilisliapas@users.noreply.github.com>
2025-10-29 00:22:19 +0200

    Added a basic JavaScript file

---
+++ static/js/game.js
@@ -0,0 +1,6 @@
+
+document.getElementById('start-game').addEventListener('click', function() {this.innerText = "Game started";});
+
+document.getElementById('start-game').addEventListener('click', function() {
+  document.getElementById('game-message').innerHTML = "<h1>Game started<h1/>";
+});
